f8077126f8aba841ef250329508cf2fec1b93256
ThunderBird2678 <edwardthetrueblade2678@gmail.com>
2017-09-17 12:29:52 -0400

    Functional
    Well, at least one menu is, at least.

--- script.js
+++ script.js
@@ -2,17 +2,124 @@ $(document).ready(
 
 	function()
 	{
-		$(".myHeader").hover(
+		$("#dropdown1").hover(
 	
+			
 			function()
 			{
-				$(".col").hide();
+				$("#dropdownmenu1").show();
 			}, 
 			
 			function()
 			{
-				$(".col").show();
+				$("#dropdownmenu1").hide();
 			}
 		);
 	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown2").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu2").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu2").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown3").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu3").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu3").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown4").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu4").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu4").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown5").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu5").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu5").hide();
+			}
+		);
+	}
+	
+);
+
+$(document).ready(
+
+	function()
+	{
+		$("#dropdown6").hover(
+	
+			
+			function()
+			{
+				$("#dropdownmenu6").show();
+			}, 
+			
+			function()
+			{
+				$("#dropdownmenu6").hide();
+			}
+		);
+	}
+	
 );
